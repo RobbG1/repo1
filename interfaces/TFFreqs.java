97b0a2f783e0ac8294dbcf8ef19b922c77db7191
RobbG1 <56204930+RobbG1@users.noreply.github.com>
2019-11-02 01:15:22 -0700

    Delete TFFreqs.java

--- interfaces/TFFreqs.java
+++
@@ -1,9 +0,0 @@
-package interfaces;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-
-public interface TFFreqs {
-    ArrayList<Map.Entry<String, Integer>> top25(List<String> words);
-}
